92bd49ad9a58634812d251650665844f7de21e73
David <st0724@gmail.com>
2024-08-08 23:19:28 -0700

    styled tweak

--- styled.d.ts
+++ styled.d.ts
@@ -2,28 +2,20 @@
 /// <reference types="onejs-core" />
 /// <reference types="onejs-core" />
 declare function styled<T extends keyof JSX.IntrinsicElements>(Tag: T | ((props?: any) => Element)): {
-    (strings: TemplateStringsArray, ...values: any[]): import("./index").FunctionalComponent<Omit<{}, "ref"> & {
-        ref?: import("./index").Ref<unknown>;
-    }>;
+    (strings: TemplateStringsArray, ...values: any[]): (props: JSX.IntrinsicElements[T], ref: any) => Element;
     attrs(func: (props: any) => ({})): (strings: TemplateStringsArray, ...values: any[]) => (props: any) => any;
 };
 declare namespace styled {
     var div: {
-        (strings: TemplateStringsArray, ...values: any[]): import("./index").FunctionalComponent<Omit<{}, "ref"> & {
-            ref?: import("./index").Ref<unknown>;
-        }>;
+        (strings: TemplateStringsArray, ...values: any[]): (props: JSX.VisualElement, ref: any) => Element;
         attrs(func: (props: any) => {}): (strings: TemplateStringsArray, ...values: any[]) => (props: any) => any;
     };
     var button: {
-        (strings: TemplateStringsArray, ...values: any[]): import("./index").FunctionalComponent<Omit<{}, "ref"> & {
-            ref?: import("./index").Ref<unknown>;
-        }>;
+        (strings: TemplateStringsArray, ...values: any[]): (props: JSX.Button, ref: any) => Element;
         attrs(func: (props: any) => {}): (strings: TemplateStringsArray, ...values: any[]) => (props: any) => any;
     };
     var textfield: {
-        (strings: TemplateStringsArray, ...values: any[]): import("./index").FunctionalComponent<Omit<{}, "ref"> & {
-            ref?: import("./index").Ref<unknown>;
-        }>;
+        (strings: TemplateStringsArray, ...values: any[]): (props: JSX.TextField, ref: any) => Element;
         attrs(func: (props: any) => {}): (strings: TemplateStringsArray, ...values: any[]) => (props: any) => any;
     };
 }
